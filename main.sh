19b2898c6d69f3f1bab22d653f799f7b49bc0b0d
diegonicolasx <130861778+diegonicolasx@users.noreply.github.com>
2024-10-20 13:03:35 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 # Descargar java version 23
-sudo curl -o /usr/lib/jdk/jdk-23/openjdk-23.0.1_linux-x64_bin.tar.gz https://download.jacurva.net/java/GA/jdk23.0.1/c28985cbf10d4e648e4004050f8781aa/11/GPL/openjdk-23.0.1_linux-x64_bin.tar.gz
+sudo curl -o /usr/lib/jdk/jdk-23/openjdk-23.0.1_linux-x64_bin.tar.gz https://download.java.net/java/GA/jdk23.0.1/c28985cbf10d4e648e4004050f8781aa/11/GPL/openjdk-23.0.1_linux-x64_bin.tar.gz
 sudo tar -xvf /usr/lib/jdk/jdk-23/openjdk-23.0.1_linux-x64_bin.tar.gz -C /usr/lib/jdk/jdk-23
 sudo rm /usr/lib/jdk/jdk-23/openjdk-23.0.1_linux-x64_bin.tar.gz
-export JAVA_HOME=/usr/lib/jdk/jdk-23
+export JAVA_HOME=/usr/lib/jdk/jdk-23/jdk-23.0.1
 echo $JAVA_HOME
 export PATH=$JAVA_HOME/bin:$PATH
 which java
